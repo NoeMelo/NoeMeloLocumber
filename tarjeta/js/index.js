0f0716c45866edefbbac9cd8b3cde2ac6285264f
noe <15101369@ue.edu.pe>
2018-08-02 02:12:23 -0500

    addd
    icono

--- tarjeta/js/index.js
+++ tarjeta/js/index.js
@@ -10,7 +10,7 @@ $(function() {
         $(this).addClass('puff-bubble');
       });
       $('#glow').remove();
-      txt.hide().html("Se <b>hará</b> realidad 😊").delay(750).fadeIn(300);
+      txt.hide().html("Se <b>hará</b> realidad 😊 ").delay(750).fadeIn(300);
       $('#candle').animate({
         'opacity': '.5'
       }, 100);
